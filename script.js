c3315f2a647b8b8d410169f150e4198a1e3871ad
Yashvanth Sankar <yashvanthsankar@gmail.com>
2022-02-16 08:46:34 +0530

    initialcommit

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-
-hamburger.addEventListener("click", show);
-
 function show() {
-  navMenu.classList.add("show");
+  document.getElementById("nav-menu").classList.toggle("show");
 }
