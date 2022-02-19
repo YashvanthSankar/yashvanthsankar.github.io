04f3594268e6839ecadc6a69ef78a9ae5c152178
Yashvanth Sankar <yashvanthsankar@gmail.com>
2022-02-19 15:08:53 +0530

    initialcommit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// Hamburger menu
 function navToggle(element) {
   element.classList.toggle("open");
 
   document.getElementById("menu").classList.toggle("hidden");
 }
+
+// Scroll to top button
+const scrollTop = document.querySelector(".scroll-top-btn");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    scrollTop.classList.add("active");
+  } else {
+    scrollTop.classList.remove("active");
+  }
+});
